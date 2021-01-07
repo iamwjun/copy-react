1ef05bb45b44ae2e55433ae3293b6f6d61aa9962
wujun <362934348@qq.com>
2021-01-07 11:17:12 +0800

    feat:define the render method

---
+++ src/copy-react/index.ts
@@ -0,0 +1,5 @@
+function render(node: any, root: any) {
+  console.log(node)
+}
+
+export { render }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from './copy-react';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <>
+    <h1>标题</h1>
+    <p>内容</p>
+  </>,
   document.getElementById('root')
 );
 
