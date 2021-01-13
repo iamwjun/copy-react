ccf2bd3789dfedf5fb08fab0c62bcb2d17c027bb
wujun <362934348@qq.com>
2021-01-13 23:00:55 +0800

    feat:实现函数组件渲染

--- src/copy-react/react-dom.ts
+++ src/copy-react/react-dom.ts
@@ -1,5 +1,12 @@
+/**
+ * vnode
+ * type 原生标签>type string
+ *      文本标签>children string number
+ *      函数组件>type function
+ * props 属性 className href id children等等
+ */
 function render(vnode: any, container: any, callback?: Function) {
-  console.log(vnode)
+  //console.log(vnode)
   const node = createNode(vnode);
   container.append(node);
 }
@@ -10,7 +17,6 @@ function isStringOrNumber(str: string | number) {
 
 function createNode(vnode: any) {
   const { type } = vnode;
-  console.log(type)
   let node;
   // todo 根据虚拟dom节点, 生成真实dom节点
   if (typeof type === 'string') {
@@ -18,13 +24,24 @@ function createNode(vnode: any) {
   } else if (isStringOrNumber(vnode)) {
     //文本数字节点
     node = updateTextComponent(vnode);
+  } else if (typeof type === 'function') {
+    updateFunctionComponent(vnode);
+  } else {
+    console.log('什么也做不了')
   }
   return node;
+}
+
+function updateNode(node: any, nextVal: any) {
+  Object.keys(nextVal).filter(k => k !== "children").forEach(k => {
+    node[k] = nextVal[k]
+  })
 }
 
 function updateHostComponent(vnode: any) {
   const { type, props } = vnode;
   const node = document.createElement(type);
+  updateNode(node, props);
   reconcileChildren(node, props.children);
   return node;
 }
@@ -39,6 +56,13 @@ function reconcileChildren(parentNode: any, children: any) {
 
 function updateTextComponent(vnode: any) {
   const node = document.createTextNode(vnode);
+  return node;
+}
+
+function updateFunctionComponent(vnode: any) {
+  const { type, props } = vnode;
+  const child = type(props);
+  const node = createNode(child);
   return node;
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,39 @@ import ReactDOM from './copy-react/react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// class ClassComponet extends React.Component {
+//   render() {
+//     return (
+//       <div>类组件</div>
+//     )
+//   }
+// }
+
+function FunctionComponent(props: any) {
+  return (
+    <div className="border">
+      <p>{props.name}</p>
+    </div>
+  )
+}
+
+const jsx = (
   <div className='demo'>
     <h1 className='title'>标题</h1>
     <p className='content'>内容</p>
     <h2 id='id'>id</h2>
-  </div>,
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+    <a href="http://www.baidu.com">链接</a>
+    <FunctionComponent name="name" />
+  </div>
+);
+
+ReactDOM.render(
+  jsx,
   document.getElementById('root')
 );
 
